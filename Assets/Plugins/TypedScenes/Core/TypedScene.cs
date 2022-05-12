105195a8344e03f012513b99c6a2b62c76183187
IRecsRu <89744179+IRecsRu@users.noreply.github.com>
2022-05-12 17:47:21 +0300

    Creating a project and adding plugin.

---
+++ Assets/Plugins/TypedScenes/Core/TypedScene.cs
@@ -0,0 +1,17 @@
+﻿using Cysharp.Threading.Tasks;
+using UnityEngine.AddressableAssets;
+using UnityEngine.ResourceManagement.ResourceProviders;
+using UnityEngine.SceneManagement;
+
+namespace IJunior.TypedScenes
+{
+    public abstract class TypedScene
+    {
+        protected static async UniTask<SceneInstance> LoadScene(string sceneName, LoadSceneMode loadSceneMode)
+        {
+            await Addressables.DownloadDependenciesAsync(sceneName);
+            SceneInstance sceneInstance = await Addressables.LoadSceneAsync(sceneName, loadSceneMode).ToUniTask();
+            return sceneInstance;
+        }
+    }
+}
